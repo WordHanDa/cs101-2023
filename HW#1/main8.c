47adbcde46adefa601ccd3127342508d603a6489
b1129012 <88619256+WordHanDa@users.noreply.github.com>
2023-03-01 01:57:44 +0800

    Update main8.c

--- HW#1/main8.c
+++ HW#1/main8.c
@@ -1,15 +1,6 @@
-/******************************************************************************
-
-Welcome to GDB Online.
-  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
-  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
-  Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
 #include <stdio.h>
 
-int main()
-{
+int main(){
     char a[]="Hello";
     int len=0;
     while(a[len]!='\0'){len++;}
