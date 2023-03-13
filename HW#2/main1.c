06734d968990997a6227ba6030801bedbc93db48
b1129012 <88619256+WordHanDa@users.noreply.github.com>
2023-03-13 11:26:29 +0800

    Update main1.c

--- HW#2/main1.c
+++ HW#2/main1.c
@@ -3,27 +3,20 @@
 #include <time.h>
 #define ARRAY_SIZE 7
 int generateRandomNumber(int previous) {
-    int num = rand() % 69 + 1;
-    while (num == previous) {num = rand() % 69 + 1;}
+    int num=rand()%69+1;
+    while (num==previous){num=rand()%69+1;}
     return num;
 }
-void generateRandomArray(int arr[]) {
+void generateRandomArray(int arr[]){
     int previous = 0;
-    for (int i = 0; i < ARRAY_SIZE - 1; i++) {
-        arr[i] = generateRandomNumber(previous);
-        previous = arr[i];
-    }
-    arr[ARRAY_SIZE - 1] = rand() % 10 + 1;
+    for(int i=0; i<ARRAY_SIZE-1; i++){arr[i]=generateRandomNumber(previous=arr[i]);}
+    arr[ARRAY_SIZE-1]=rand()%10+1;
 }
 char* date(){
-    char timezone[] = "TZ=Asia/Taipei";
-    putenv(timezone);
+    putenv("TZ=Asia/Taipei");
     tzset();
-    time_t current_time;
-    struct tm *local_time;
-    current_time = time(NULL);
-    local_time = localtime(&current_time);
-    return asctime(local_time);
+    time_t current_time=time(NULL);
+    return asctime(localtime(&current_time));
 }
 int main(){
     char lotto[1000];
@@ -32,19 +25,15 @@ int main(){
     FILE*fp=fopen("lotto.txt","w");
     printf("歡迎光臨長庚樂透購買機台\n請問您要買幾組樂透彩:");
     scanf("%d",&x);
-    printf("已為您購買的 %d 組樂透組合輸出至lotto.txt\n",x);
-    fprintf(fp,"========lotto649========\n");
-    fprintf(fp,"%s",date());
-    for(j=0; j<x; j++) {
+    printf("已為您購買的%d組樂透組合輸出至lotto.txt\n",x);
+    fprintf(fp,"========lotto649========\n%s",date());
+    for(j=0; j<x; j++){
         generateRandomArray(arr);
         fprintf(fp,"[%d]: ", j+1);
-        for(int i = 0; i < ARRAY_SIZE; i++) {fprintf(fp,"%d ", arr[i]);}
+        for(int i = 0; i<ARRAY_SIZE; i++) {fprintf(fp,"%d ", arr[i]);}
         fprintf(fp,"\n");
     }
-    for(k=j; k<5;k++){
-        fprintf(fp,"[%d]: ", k+1);
-        fprintf(fp,"-- -- -- -- -- -- -\n");
-    }
+    for(k=j; k<5;k++){fprintf(fp,"[%d]: -- -- -- -- -- -- --\n", k+1);}
     fprintf(fp,"========csie@cgu========");
     fclose(fp);
 }
